f6bc418c151b21f2c868491980c96c979a991976
Diogo Cardoso <diogomescardoso@gmail.com>
2025-10-30 16:58:43 -0300

    refactor: modify CORS configuration to allow all origins

--- backend/src/main/java/com/portifolio/todolist/configuration/CorsConfiguration.java
+++ backend/src/main/java/com/portifolio/todolist/configuration/CorsConfiguration.java
@@ -9,7 +9,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:3000",
+                                "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
     }
 }
