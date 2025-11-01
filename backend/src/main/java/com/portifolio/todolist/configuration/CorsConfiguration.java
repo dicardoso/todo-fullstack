12d3254f1e560ff03877f95cfcbac4c84a2bdda4
Diogo Cardoso <diogomescardoso@gmail.com>
2025-11-01 10:03:20 -0300

    fix: update CORS configuration to include additional allowed origin

--- backend/src/main/java/com/portifolio/todolist/configuration/CorsConfiguration.java
+++ backend/src/main/java/com/portifolio/todolist/configuration/CorsConfiguration.java
@@ -10,7 +10,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000",
-                                "http://localhost:5173")
+                                "http://localhost:5173",
+                                "https://todo-fullstack-rose.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
     }
 }
